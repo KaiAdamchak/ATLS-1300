8198f51f0b370504aea9001390117e94ceba01db
KaiAdamchak <111911879+KaiAdamchak@users.noreply.github.com>
2022-09-01 21:43:14 -0600

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+
+name = input("What's Your Name?  ")
+print(name, "right?")
+age = input("How old are you?  ")
+print(age, "WOW! I dont know how old I am, but computers were made in 1996 so maybe I'm 26.")
