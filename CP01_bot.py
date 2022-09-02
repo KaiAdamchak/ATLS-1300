4edef9517dc8adbecc1167687d6ee258fa549150
KaiAdamchak <111911879+KaiAdamchak@users.noreply.github.com>
2022-09-02 11:54:43 -0600

    Create CP01_bot.py

---
+++ CP01_bot.py
@@ -0,0 +1,11 @@
+#conversation with patient code
+print("Hello! I'm Baemax, your personal healthcare assistant.")
+name = input("What is your name?  ")
+print("Patient name, ", name, ", stored in memory.")
+feeling_value = input("On a scale from 1 to 10 how are you feeling today? ")
+print(feeling_value, "?")
+symptoms = input("What symptoms are leading to you feeling this way?  ")
+print("Symptoms", symptoms, "saved to patient file,", name, ".")
+print("I recommend rest, water, and fun to releave your symtoms.")
+ending = input("To turn off Baemax respond with I am satisfied with my care...  ")
+print("You are satisfied with your care, good bye.")
